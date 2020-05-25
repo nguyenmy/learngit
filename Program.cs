8c1ccfcd0f0cb9cf33548d251aee7d7deecbe41c
HUONG <HUONG@DESKTOP-9K4ASPG>
2020-05-26 06:14:22 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
